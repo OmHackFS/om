2b498b845c387848427b9622a473c1e0124cbbc8
Eric Le <therealericle@gmail.com>
2022-07-09 20:58:59 -0700

    add logo, Header, SidebarNavigation and SubHeader

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,10 +2,12 @@ import { ReactElement } from 'react';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Wallet } from './components/Wallet';
 import { MainPage } from './components/MainPage';
+import { SidebarNavigation } from './components/SidebarNavigation';
 
 export function App(): ReactElement {
   return (
     <div className="App">
+      <SidebarNavigation />
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<MainPage />} />
--- frontend/src/components/ConnectWalletButton.tsx
+++ frontend/src/components/ConnectWalletButton.tsx
@@ -28,15 +28,15 @@ export const ConnectWalletButton = () => {
   return (
     <button
       onClick={handleClick}
-      className="group relative flex w-48 justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-[#F55C40] hover:bg-[#F55C40] focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-[#F55C40]"
+      className="group relative flex w-48 justify-center py-2 border border-transparent text-sm font-medium rounded-md text-black bg-[#] hover:bg-[#] focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-[#]"
     >
       <span className="absolute left-0 inset-y-0 flex items-center pl-3">
         <LinkIcon
-          className="h-5 w-5 text-white group-hover:text-white"
+          className="h-5 w-5 text-black group-hover:text-black"
           aria-hidden="true"
         />
       </span>
-      Connect to wallet
+      Connect Wallet
     </button>
   );
 };
