eddd7e4d6d4856c62d5b48da543d39eddda4eca2
writersblockchain <seanmconrad94@gmail.com>
2022-07-12 15:43:33 -0400

    soul bound token contract + hardhat config

---
+++ test/OmSbToken.ts
@@ -0,0 +1,12 @@
+import { expect } from "chai";
+import hre from "hardhat";
+import helpers from "@nomicfoundation/hardhat-network-helpers";
+
+describe("safeMint", function () {
+  it("Should mint Om soulbound token", async function () {
+   
+  });
+});
+
+
+// npx hardhat test test/OmSbToken.ts
