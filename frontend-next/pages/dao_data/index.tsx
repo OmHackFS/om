b0905edddc94648b766ba15ecbebb3581fbe47b9
danilowhk <danilowhk@gmail.com>
2022-07-24 02:00:40 -0300

    dao_Data page

--- frontend-next/pages/dao_data/index.tsx
+++ frontend-next/pages/dao_data/index.tsx
@@ -46,7 +46,7 @@ export default function DaoDataPage({data2} :any) {
             </div>
             <SubHeader />
             <div className="max-w-7xl mx-auto px-4 sm:px-6 md:px-8">
-            <DaoData data="data"/>
+            <DaoData data={data2}/>
  
             </div>
           </div>
