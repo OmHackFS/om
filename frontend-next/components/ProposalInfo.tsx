2738bf0eaf09bd9085f324112c70d85fd8261f0b
danilowhk <danilowhk@gmail.com>
2022-07-24 00:28:38 -0300

    vote

--- frontend-next/components/ProposalInfo.tsx
+++ frontend-next/components/ProposalInfo.tsx
@@ -15,6 +15,8 @@ import { BellIcon, MenuIcon, XIcon } from "@heroicons/react/outline";
 import { proposalsList } from "./mocks/proposals";
 import { ProofModalVote } from "./ProofModalVote";
 import { useState } from "react";
+import { Listbox } from '@headlessui/react'
+import { SelectorIcon } from '@heroicons/react/solid'
 
 const user = {
   name: "Whitney Francis",
@@ -31,44 +33,20 @@ const eventTypes = {
 const timeline = [
   {
     id: 1,
-    type: eventTypes.applied,
-    content: "Voting Started",
-    target: "By: Sub-Group1",
-    date: "Sep 20",
-    datetime: "2020-09-20",
+    type: eventTypes.completed,
+    content: "Yes",
+    target: "25 votes",
+    date: "Sep 28",
+    datetime: "2020-09-28",
   },
   {
     id: 2,
     type: eventTypes.advanced,
     content: "No",
-    target: "Sub-Group2",
+    target: "10 votes",
     date: "Sep 22",
     datetime: "2020-09-22",
-  },
-  {
-    id: 3,
-    type: eventTypes.completed,
-    content: "Yes",
-    target: "Martha Gardner",
-    date: "Sep 28",
-    datetime: "2020-09-28",
-  },
-  {
-    id: 4,
-    type: eventTypes.advanced,
-    content: "No",
-    target: "Bethany Blake",
-    date: "Sep 30",
-    datetime: "2020-09-30",
-  },
-  {
-    id: 5,
-    type: eventTypes.completed,
-    content: "Yes",
-    target: "Sub Group 4",
-    date: "Oct 4",
-    datetime: "2020-10-04",
-  },
+  }
 ];
 const comments = [
   {
@@ -94,6 +72,11 @@ const comments = [
   },
 ];
 
+const people = [
+  { id: 1, name: 'Yes' },
+  { id: 2, name: 'No' },
+]
+
 function classNames(...classes: any[]) {
   return classes.filter(Boolean).join(" ");
 }
@@ -104,6 +87,8 @@ type ProposalInfoProps = {
 
 export const ProposalInfo = ({ proposalId }: ProposalInfoProps) => {
   const [showProposalModal, setShowProposalModal] = useState<boolean>(false);
+  const [selected, setSelected] = useState<any>(people[0])
+
 
   const handleShowProposalModal = () => setShowProposalModal(true);
 
@@ -405,6 +390,63 @@ export const ProposalInfo = ({ proposalId }: ProposalInfoProps) => {
                     ))}
                   </ul>
                 </div>
+                <Listbox value={selected} onChange={setSelected}>
+      {({ open }) => (
+        <>
+          <Listbox.Label className="mt-3 block text-sm font-medium text-gray-700">Select Vote</Listbox.Label>
+          <div className="mt-1 relative">
+            <Listbox.Button className="bg-white relative w-full border border-gray-300 rounded-md shadow-sm pl-3 pr-10 py-2 text-left cursor-default focus:outline-none focus:ring-1 focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm">
+              <span className="block truncate">{selected.name}</span>
+              <span className="absolute inset-y-0 right-0 flex items-center pr-2 pointer-events-none">
+                <SelectorIcon className="h-5 w-5 text-gray-400" aria-hidden="true" />
+              </span>
+            </Listbox.Button>
+
+            <Transition
+              show={open}
+              as={Fragment}
+              leave="transition ease-in duration-100"
+              leaveFrom="opacity-100"
+              leaveTo="opacity-0"
+            >
+              <Listbox.Options className="absolute z-10 mt-1 w-full bg-white shadow-lg max-h-60 rounded-md py-1 text-base ring-1 ring-black ring-opacity-5 overflow-auto focus:outline-none sm:text-sm">
+                {people.map((person) => (
+                  <Listbox.Option
+                    key={person.id}
+                    className={({ active }) =>
+                      classNames(
+                        active ? 'text-white bg-indigo-600' : 'text-gray-900',
+                        'cursor-default select-none relative py-2 pl-3 pr-9'
+                      )
+                    }
+                    value={person}
+                  >
+                    {({ selected, active }) => (
+                      <>
+                        <span className={classNames(selected ? 'font-semibold' : 'font-normal', 'block truncate')}>
+                          {person.name}
+                        </span>
+
+                        {selected ? (
+                          <span
+                            className={classNames(
+                              active ? 'text-white' : 'text-indigo-600',
+                              'absolute inset-y-0 right-0 flex items-center pr-4'
+                            )}
+                          >
+                            <CheckIcon className="h-5 w-5" aria-hidden="true" />
+                          </span>
+                        ) : null}
+                      </>
+                    )}
+                  </Listbox.Option>
+                ))}
+              </Listbox.Options>
+            </Transition>
+          </div>
+        </>
+      )}
+    </Listbox>
                 <div className="mt-6 flex flex-col justify-stretch">
                   <button
                     type="button"
